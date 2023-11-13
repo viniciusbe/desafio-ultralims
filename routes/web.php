c40a4740c49d8dde019639a06ba9aa2218145e4b
Vinícius Bernardes <vinicius@bernardes.dev>
2023-11-13 02:51:07 -0300

    chore: remove controller useless function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AddressController::class, 'index']);
+Route::get('/', function () {
+    return view('home');
+});
